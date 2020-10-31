0f0123061872d6bc8d6b4c94ea9c2ed4c6586df8
kkoon9 <rndrnjs2003@gmail.com>
2020-10-31 21:55:03 +0900

    feat : pdf to image and upload object storage(s3)
    - use node-poppler

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import pdf from 'pdf-poppler';
 import fs from 'fs';
 import rimraf from 'rimraf';
+import { Poppler } from 'node-poppler';
 import getArray from '../modules/getArray';
 import upload from '../modules/multer';
 
@@ -9,28 +10,28 @@ const router = Router();
 
 router.post('/upload', upload.any(), async (req, res) => {
   const { filename } = req.files[0];
+  const poppler = new Poppler();
   const filePath = filename.split('.pdf')[0];
   const file = `./src/uploads/${filename}`;
-  const opts = {
-    format: 'png',
-    out_dir: `${__dirname}/../${filePath}`,
-    out_prefix: filePath,
-    page: null,
-    combinedImage: true
+  const options = {
+    firstPageToConvert: 1,
+    pngFile: true,
   };
-  try {
-    fs.mkdirSync(`${__dirname}/../${filePath}`);
-  } catch (e) {
-    if (e.code !== 'EEXIST') throw e;
-  }
-  await pdf.convert(file, opts)
+
+  const outputFile = `${__dirname}/../uploads/test`;
+  await poppler.pdfToCairo(options, file, outputFile)
     .then(async () => {
-      // console.log(`${filePath} Successfully converted`);
+      fs.unlink(file, (err) => {
+        if (err) throw err;
+      });
     })
     .then(async () => {
-      const result = await getArray(filePath);
+      const result = await getArray('uploads');
       // 디렉터리 삭제
-      rimraf(`${__dirname}/../${filePath}`);
+      rimraf(`${__dirname}/../uploads`, () => {
+        console.log('done');
+      });
+      console.log(result);
       res.json({
         success: true,
         data: result
